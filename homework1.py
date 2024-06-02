6874812679e8d097a03a3bb1333772bde5e0448e
Olga5515 <torfenita@mail.ru>
2024-06-02 10:22:37 +1000

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+example = 'Коллаборация'
+print(example[0])
+print(example[-1])
+print(example[7:])
+print(example[::-1])
+print(example[1::2])  # пример в п.6 Задания и образец выполнения - противоречат друг другу
+  # отправлено 2024/06/02 10:20
